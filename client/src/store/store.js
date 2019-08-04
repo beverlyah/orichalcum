ea4a4c908c8d2ca16fe2e819f689e0c6e5e2ec48
Brett Cunningham <bwcunninghamii@gmail.com>
2019-08-04 17:08:14 -0400

    test

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/main.js";
+// test
 import thunkMiddleWare from "redux-thunk";
 
 var store = createStore(
